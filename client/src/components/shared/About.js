9aa674f07a02de66bc20222b97f2fd8be7250ff2
Samantha Malay <samaleemalay@gmail.com>
2019-10-07 20:13:54 -0600

    updating about

--- client/src/components/shared/About.js
+++ client/src/components/shared/About.js
@@ -7,9 +7,15 @@ const About = () => {
   return (
   
     <div class="main">
+      <div class='logo'>Logo Here</div>
       <div class="title">Learn More About Generations Humanitarian</div>
-      <div class="mission">Our Mission</div>
-      <div class="miss-paragraph">Generations Humanitarian is committed to transforming street children toward an abundance of health, hope, and loving homes—thereby breaking the cycle of scarcity for generations to come. This is done through broad research, novel development, strengthening nonprofit entities in development, and individual nurturing in a home environment.</div>
+      <div class="mission">OUR MISSION</div>
+      <div class="mission-paragraph">Generations Humanitarian is committed to transforming street children toward an abundance of health, hope, and loving homes—thereby breaking the cycle of scarcity for generations to come. This is done through broad research, novel development, strengthening nonprofit entities in development, and individual nurturing in a home environment.</div>
+    </div>
+
+    <div class="bottom">
+      <div class="converted">BECOME CONVERTED</div>
+      <div class="converted-paragraph">Come with us to Peru or Colombia and know these children first hand. Strip away the materialism that surrounds us here in the USA and experience how these children view the world—their humility, authenticity, and love. Over time, those who have come on expeditions join this meaningful battle to save these kids and give them life. Most comment how happy it makes them. There are many worthy causes in the world today—arts, environment, animals, and public benefit—but no greater joy comes from loving our fellowman and serving the “least of these.”</div>
     </div>
 
   )
