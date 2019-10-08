42752c9975e2b387ecc2ab833772c315fd35a522
Chelsie Conrad <chelsiechey@gmail.com>
2019-10-07 20:48:25 -0600

    pulling down

--- client/src/components/shared/Navbar.jsx
+++ client/src/components/shared/Navbar.jsx
@@ -6,15 +6,17 @@ const Navbar = () => {
   return(
     <>
     <div className='main-nav'>
-      <div className='left-nav'>
-        <NavLink to="/" className='nav-item'>Home</NavLink>
-      </div>
-      <div className='right-nav'>
-        <NavLink to="/calendar" className='nav-item' >Calendar</NavLink>
-        <NavLink to="/about" className='nav-item'>About Us</NavLink>
-        <NavLink to="/expeditions" className='nav-item'>Expeditions</NavLink>
-        <NavLink to="/homes" className='nav-item'>Homes & Projects</NavLink>
-        <NavLink to="/contact" className='nav-item'>Contact Us</NavLink>
+      <div className='nav_column'>
+        <div className='left_column'>
+          <NavLink to="/" className='nav-item'>Home</NavLink>
+        </div>
+        <div className='right_column'>
+          <NavLink to="/calendar" className='nav-item' >Calendar</NavLink>
+          <NavLink to="/about" className='nav-item'>About Us</NavLink>
+          <NavLink to="/expeditions" className='nav-item'>Expeditions</NavLink>
+          <NavLink to="/homes" className='nav-item'>Homes & Projects</NavLink>
+          <NavLink to="/contact" className='nav-item'>Contact Us</NavLink>
+        </div>
       </div>
     </div>
     </>
