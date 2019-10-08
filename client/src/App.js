39bbbb789b0d3146ac66cf6f9ef1b44d15f9ee76
Harlan Evans <35747338+harlanevans@users.noreply.github.com>
2019-10-07 20:40:46 -0600

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Fragment, } from 'react';
 import Home from './components/shared/Landing';
 import Nomatch from './components/shared/Nomatch';
@@ -9,22 +8,10 @@ import { Switch, Route, } from 'react-router-dom';
 import AdminHome from './components/admin/AdminHome';
 import ProtectedRoute from './components/auth/ProtectedRoute';
 // import About from './components/shared/About';
-=======
-import React, { Fragment } from "react";
-import Home from "./components/shared/Landing";
-import Nomatch from "./components/shared/Nomatch";
-import FetchUser from "./components/auth/FetchUser";
-import Login from "./components/auth/Login";
-import Register from "./components/auth/Register";
-import { Switch, Route } from "react-router-dom";
-import AdminHome from "./components/admin/AdminHome";
-import ProtectedRoute from "./components/auth/ProtectedRoute";
->>>>>>> 338fe5da7a985c7841072cd138d8b9b9d2cfd545
 
 const App = () => (
   // if the screen size changes show this:
   <Fragment>
-<<<<<<< HEAD
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/admin" component={Login} />
@@ -34,17 +21,6 @@ const App = () => (
         </FetchUser>
         <Route component={Nomatch} />
       </Switch>
-=======
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/admin" component={Login} />
-      <Route exact path="/register" component={Register} />
-      <FetchUser>
-        <ProtectedRoute exact path="/admin_home" component={AdminHome} />
-      </FetchUser>
-      <Route component={Nomatch} />
-    </Switch>
->>>>>>> 338fe5da7a985c7841072cd138d8b9b9d2cfd545
   </Fragment>
 );
 
