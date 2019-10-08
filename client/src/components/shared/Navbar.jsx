6fcbac031eba18b57ca0ffa9f972ed67e0fd4879
Chelsie Conrad <chelsiechey@gmail.com>
2019-10-07 21:15:43 -0600

    navbar

--- client/src/components/shared/Navbar.jsx
+++ client/src/components/shared/Navbar.jsx
@@ -4,24 +4,18 @@ import styles from'./Main.css';
 
 const Navbar = () => {
   return(
-    <>
     <div className='main-nav'>
-      <div className='nav_column'>
-        <div className='left_column'>
-          <NavLink to="/" className='nav-item'>Home</NavLink>
-        </div>
+      <div className='nav-home'>
+        <NavLink to="/" className='nav-item'>Home</NavLink>
       </div>
-      <div className='nav_column'>
-        <div className='right_column'>
-          <NavLink to="/calendar" className='nav-item' >Calendar</NavLink>
-          <NavLink to="/about" className='nav-item'>About Us</NavLink>
-          <NavLink to="/expeditions" className='nav-item'>Expeditions</NavLink>
-          <NavLink to="/homes" className='nav-item'>Homes & Projects</NavLink>
-          <NavLink to="/contact" className='nav-item'>Contact Us</NavLink>
-        </div>
+      <div className='right_nav'>
+        <NavLink to="/calendar" className='nav-item' >Calendar</NavLink>
+        <NavLink to="/about" className='nav-item'>About Us</NavLink>
+        <NavLink to="/expeditions" className='nav-item'>Expeditions</NavLink>
+        <NavLink to="/homes" className='nav-item'>Homes & Projects</NavLink>
+        <NavLink to="/contact" className='nav-item'>Contact Us</NavLink>
       </div>
     </div>
-    </>
   )
 }
 
