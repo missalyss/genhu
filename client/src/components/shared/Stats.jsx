ef9cb196e7f3a0a6a683b3666b1ec4be80176edb
Christian Cuartas <christian.cuartas@gmail.com>
2019-10-07 20:49:37 -0600

    more updates to stats

--- client/src/components/shared/Stats.jsx
+++ client/src/components/shared/Stats.jsx
@@ -4,11 +4,8 @@ import React from 'react';
 const Stats = () => {
   return(
     <div class='body'>
-      <div class='statsHeader'>SUCCESS 
-      <br/>
-      <br/> 
-      Since 2008. You have cared. We did the math.
-      <br/><br/>
+      <div class='statsHeader'><h1 style={{marginTop: '5rem'}}>SUCCESS </h1>
+      <h1>Since 2008. You have cared. We did the math.</h1>
       </div>
       <div class="container">
         <div class="cell cell-1"><h1>1526</h1><h6>Children</h6></div>
@@ -16,9 +13,9 @@ const Stats = () => {
         <div class="cell cell-3"><h1>818,033</h1><h6>Meals To Date</h6></div>
         <div class="cell cell-4"><h1>18.05</h1><h6>Tons of Donations</h6></div>
         <div class="cell cell-5"><h1>90,136</h1><h6>Volunteer Hours</h6></div>
-        <div class="cell cell-6">507<br/>  Expedition Participants</div>
+        <div class="cell cell-6"><h1>507</h1><h6> Expedition Participants</h6></div>
         <div class="cell cell-7"></div>
-        <div class="cell cell-7">$2,175,883<br/>  Value of Volunteer Time</div>
+        <div class="cell cell-7"><h1>$2,175,883</h1><h6>Value of Volunteer Time</h6> </div>
         <div class="cell cell-7"></div>
       </div>
     </div>
