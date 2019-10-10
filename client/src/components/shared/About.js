839612ff9dace065cf23af8a9021c6b9609f4a65
Samantha Malay <samaleemalay@gmail.com>
2019-10-09 18:48:58 -0600

    carousel skeleton

--- client/src/components/shared/About.js
+++ client/src/components/shared/About.js
@@ -1,7 +1,8 @@
 import React from "react";
-// import Logo from '../../assets/Images/LogoSymbol.png';
 import Carousel from 'react-bootstrap/Carousel';
 import './About.css';
+
+
 
 const About = () => {
 
@@ -10,10 +11,10 @@ const About = () => {
       {/* <div class="title">Learn More About Generations Humanitarian</div> */}
 
       <div class="main">
-          <div class="container">
-            <div class="logo"></div>
+          <div class="about-container">
+            <div class="about-logo"></div>
             <div class='learn'>
-            <div class="title">Learn More About Generations Humanitarian</div>
+            <div class="about-title">Learn More About Generations Humanitarian</div>
             <div class="mission">OUR MISSION</div>
             <div class="mission-paragraph">
               Generations Humanitarian is committed to transforming street children
@@ -27,28 +28,40 @@ const About = () => {
         </div>
 
       <div class="bottom">
-        <div class="converted">BECOME CONVERTED</div>
-        <div class="converted-paragraph">
-          Come with us to Peru or Colombia and know these children first hand.
-          Strip away the materialism that surrounds us here in the USA and
-          experience how these children view the world—their humility,
-          authenticity, and love. Over time, those who have come on expeditions
-          join this meaningful battle to save these kids and give them life.
-          Most comment how happy it makes them. There are many worthy causes in
-          the world today—arts, environment, animals, and public benefit—but no
-          greater joy comes from loving our fellowman and serving the “least of
-          these.”
-        </div>
-          <div class="photos-col">
-            agageoja
-          <div class="carousel">
-            <Carousel>
-            
-            </Carousel>
+        <div className="convert-container">
+          <div class="converted">BECOME CONVERTED</div>
+          <div class="converted-paragraph">
+            Come with us to Peru or Colombia and know these children first hand.
+            Strip away the materialism that surrounds us here in the USA and
+            experience how these children view the world—their humility,
+            authenticity, and love. Over time, those who have come on expeditions
+            join this meaningful battle to save these kids and give them life.
+            Most comment how happy it makes them. There are many worthy causes in
+            the world today—arts, environment, animals, and public benefit—but no
+            greater joy comes from loving our fellowman and serving the “least of
+            these.”
           </div>
         </div>
       </div>
     </div>
+  );
+};
+
+
+export const AboutCarousel = () => {
+
+  return(
+
+  <div class="bottom">
+
+    
+    <Carousel className='about-wrapper' fade indicators={false} interval={2500} pauseOnHover={false}>
+      <Carousel.Item className='about-pic1'>
+        jioejioegajioaegio;aegoarge
+      </Carousel.Item>
+    </Carousel>
+    
+  </div>
   );
 };
 
--- client/src/components/shared/Landing.jsx
+++ client/src/components/shared/Landing.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { GlobalTitle } from './GlobalStyles';
-import Navbar from './Navbar'
-import About from './About';
+import Navbar from './Navbar';
+import About from '../shared/About';
+import './About.css';
+import { AboutCarousel } from '../shared/About';
 import LandingCarousel from './Carousel';
 import Stats from '../shared/Stats'
 import ProvenSouls from '../shared/Proved-souls';
@@ -16,6 +18,7 @@ const Landing = () => {
      <ProvenSouls />
      <Stats/>
     <About />
+      <AboutCarousel />
     </div>
     )
 }
