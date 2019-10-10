6803779ba5fec24e563c20a5afe9159982b476c8
Christian Cuartas <christian.cuartas@gmail.com>
2019-10-09 19:41:18 -0600

    Completed footer

---
+++ client/src/components/shared/Footer.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import "./Footer.css"
+
+
+const Stats = () => {
+  return(
+    <div class='footer-container'>
+      <div class="row">
+        <div class="column">
+          <h2 class= 'header'>OUR MISSION</h2>
+          <p class='mission'>Generations Humanitarian is committed to transform street children toward an abundance of health,
+             hope, and loving homes—thereby breaking the cycle of scarcity for generations to come. 
+             This is done through broad research, novel development, strengthening nonprofit entities in development, 
+             and individual nurturing in a home environment.</p>
+        </div>
+        <div class="column">
+          <h2 class= 'header'>GET INVOLVED</h2>
+          <p>Go on an expedition</p>
+          <p>Gift Levels:</p>
+          <p>Support Sponsor: $50-100 monthly</p>
+          <p>Research Sponsor: $250 monthly</p>
+          <p>Home Nurturing Sponsor: $1500+ monthly</p>
+          <p>Volunteer and Fundraise</p>
+        </div>
+        <div class="column">
+          <h2 class= 'header'>LEGAL STUFF</h2>
+          <p>Policy and Financial</p>
+          <p>Privacy</p>
+        </div>
+        <div class="column">
+          <h2 class= 'header'>CONTACT US</h2>
+          <p>2607 Casto Lane,</p>
+          <p>Salt Lake City, Utah 84117</p>
+          <p>Phone: +1 801-859-1033</p>
+          <p>E-mail: van@genhu.org</p>
+          <p>Website: www.genhu.org</p>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Stats;
+
