44eb11713c79b3e259bf4850df0c829edf6dc877
Chelsie Conrad <chelsiechey@gmail.com>
2019-10-10 20:38:38 -0600

    pulling down

--- client/src/components/contact/Contact.js
+++ client/src/components/contact/Contact.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import Logo from "../../assets/Images/GENHU-logo-fake.png";
+import styles from'../shared/Main.css';
 
 class Contact extends Component {
   render() {
     return (
       <>
-      <h2>Coming Soon!</h2>
+      <div className='contact'>
+      <div className='contact_left'>
+      <img src={Logo} className='contact_logo' alternate='logo'/>
+      <div className='address_info'>
+      <p>2607 Casto Lane,</p>
+      <p>Salt Lake City, Utah 84117</p>
+      <p>Phone: +1 801-859-1033</p>
+      <p>E-mail: van@genhu.org</p>
+      <p>Website: www.genhu.org</p>
+      </div>
+      </div>
+      <div className='contact_right'>
+        <p>Insert form here</p>
+      </div>
+      </div>
       </>
     )
   }
