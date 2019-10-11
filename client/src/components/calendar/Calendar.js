dc0749ead1cceddcd92085d98c705b4ed02b1bd3
Christian Cuartas <christian.cuartas@gmail.com>
2019-10-10 18:30:38 -0600

    added footer to additional pages

--- client/src/components/calendar/Calendar.js
+++ client/src/components/calendar/Calendar.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Footer from '../shared/Footer';
 
 class Calendar extends Component {
   render() {
     return (
       <>
       <h2>Coming Soon!</h2>
+      <Footer/>
       </>
     )
   }
--- client/src/components/contact/Contact.js
+++ client/src/components/contact/Contact.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import Footer from '../shared/Footer';
 
 class Contact extends Component {
   render() {
     return (
+      
       <>
       <h2>Coming Soon!</h2>
+      <Footer/>
       </>
+      
+  
     )
   }
 }
