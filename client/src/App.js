01f5a9d8f62e85ac17c6e4185439fc994a7c3ad3
Harlan Evans <harlanevans5@gmail.com>
2019-10-10 18:22:29 -0600

    move nomatch

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,9 @@ const App = () => (
     <Navbar />
     <>
       <Switch>
+        <FetchUser>
+          <ProtectedRoute exact path="/admin_home" component={AdminHome} />
+        </FetchUser>
         <Route exact path="/" component={Home} />
         <Route exact path="/admin" component={Login} />
         <Route exact path="/register" component={Register} />
@@ -28,9 +31,6 @@ const App = () => (
         <Route exact path='/expeditions' component={Expeditions} />
         <Route exact path='/homes' component={Homes} />
         <Route exact path='/contact' component={Contact} />
-        <FetchUser>
-          <ProtectedRoute exact path="/admin_home" component={AdminHome} />
-        </FetchUser>
         <Route component={Nomatch} />
       </Switch>
       </>
