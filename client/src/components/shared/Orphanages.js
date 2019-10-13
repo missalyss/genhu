ca8bc9f189eae1ef52667ff9657cdf1488796b50
Samantha Malay <samaleemalay@gmail.com>
2019-10-13 16:53:22 -0600

    updated syntax

--- client/src/components/shared/Orphanages.js
+++ client/src/components/shared/Orphanages.js
@@ -18,13 +18,14 @@ const Orphanages = () => {
         <div classNam="mid-p">INSERT TEXT HERE: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
       </div>
 
-      <div className="mid-container">
+      <div className="mid-container1">
         <div className="mid-box1">Headline
         <div className="mid-para1">INSERT TEXT HERE: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </div>
         </div>
         <div className="mid-picbox1"> Picture Here</div>
       </div>
+      
       <div className="mid-container2">
         <div className="mid-picbox1"> Picture Here</div>
         <div className="mid-box2">Headline
