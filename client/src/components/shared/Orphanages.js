6a86a08aa4f13d441b954cc0b9f2ab82b5e3177d
Samantha Malay <samaleemalay@gmail.com>
2019-10-13 16:50:14 -0600

    unfinished skeleton of orphanges page

---
+++ client/src/components/shared/Orphanages.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './Orphanages.css';
+// import Photo from '../../assets/Images/testpic';
+
+
+const Orphanages = () => {
+  
+  return (
+    <div>
+      <div className="orph-background">
+        {/* <img scc={Photo} className='testpic'/> */}
+        <div className="orph-title">About Generations Humanitarian Orphanages</div>
+        {/* <img src={Photo} className='background'/> */}
+      </div>
+
+      <div className="mid-col">
+        <div className="mid-title">Headline</div>
+        <div classNam="mid-p">INSERT TEXT HERE: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+      </div>
+
+      <div className="mid-container">
+        <div className="mid-box1">Headline
+        <div className="mid-para1">INSERT TEXT HERE: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+        </div>
+        <div className="mid-picbox1"> Picture Here</div>
+      </div>
+      <div className="mid-container2">
+        <div className="mid-picbox1"> Picture Here</div>
+        <div className="mid-box2">Headline
+        <div className="mid-para2">INSERT TEXT HERE: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+        </div>
+      </div>
+    
+    </div>
+    
+    
+    
+  )
+}
+
+export default Orphanages;
