65c7f1ef35762f31d4f7c5549ea82ad4eb1864a4
Samantha Malay <samaleemalay@gmail.com>
2019-10-15 00:12:42 -0600

    updated photos

--- client/src/components/orphanages/Orphanages.js
+++ client/src/components/orphanages/Orphanages.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Orphanages.css';
+import Footer from '../shared/Footer';
 // import TestPic from '../../assets/Images/testpic';
 
 
@@ -26,15 +27,15 @@ const Orphanages = () => {
       </div>
       
       <div className="mid-container2">
-        <div className="mid-picbox1"> Picture Here</div>
+        <div className="mid-picbox2"> Picture Here</div>
         <div className="mid-box2">Headline
         <div className="mid-para2">INSERT TEXT HERE: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </div>
         </div>
       </div>
     
+      <Footer />
     </div>
-    
     
     
   )
