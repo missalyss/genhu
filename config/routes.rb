faeb1dbdd85773002c02b3c1a30d9f91ae2a3caa
Harlan Evans <35747338+harlanevans@users.noreply.github.com>
2019-10-17 18:49:02 -0600

    Update routes.rb
    updated merge conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -6,17 +6,16 @@ Rails.application.routes.draw do
   end
   
   namespace :api do
-<<<<<<< HEAD
     resources :employees
     get '/volunteers/', to: 'employees#volunteers'
     get '/staff/', to: 'employees#staff'
     get '/directors', to: 'employees#directors'
-=======
+  end
+  namespace :api do
     resources :homes
     get '/orphanages/', to: 'homes#orphanages'
     get '/centers/', to: 'homes#centers'
     # get '/homes/', to: 'homes#index'
->>>>>>> 3dee6c58fdaaf1fa5f140f2405ee477be079d521
   end
 
   # Custom Routes for Homes
