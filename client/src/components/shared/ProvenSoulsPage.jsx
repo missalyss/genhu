df09b4db52c5a8d505713aa61e4b512325bb0aac
Chelsie Conrad <chelsiechey@gmail.com>
2019-10-23 22:41:18 -0600

    pulling

--- client/src/components/shared/ProvenSoulsPage.jsx
+++ client/src/components/shared/ProvenSoulsPage.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Title, SubHeading, Button } from '../styles/StyledComponents';
+import Footer from '../shared/Footer';
 
 
-const ProvenSouls = () => {
+const ProvenSoulsPage = () => {
   return (
   <>
-    <div style={{padding: "100px 0px 0px 50px"}}>
+    <div style={{padding: "100px 0px 600px 50px"}}>
       <h1>Coming Soon!</h1>
     </div>
+    <Footer />
   </>
   )
 }
 
-export default ProvenSouls;
+export default ProvenSoulsPage;
