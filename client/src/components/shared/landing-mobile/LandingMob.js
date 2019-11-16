48c8397432d159dbd10f274730ed52b81bf6d621
Taylor Covington <tcovingtondev@gmail.com>
2019-11-16 15:13:20 -0700

    added folders for carousel mobile, commiting to pull master

---
+++ client/src/components/shared/landing-mobile/LandingMob.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import CarouselMob from './carousel-mob/CarouselMob';
+
+const LandingMob = () => (
+  <>
+    <NavMob />
+    <CarouselMob />
+  </>
+)
+
+export default LandingMob;
---
+++ client/src/components/shared/landing-mobile/carousel-mob/CarouselMob.jsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import { Title, SubHeading, Button } from '../styles/StyledComponents';
+import './CarouselMob.css';
+import { HashLink as Link } from 'react-router-hash-link';
+
+// style={{border: 'solid 1px blue'}}
+
+const LandingCarousel = () => {
+  return(
+    <Carousel className='hero-wrapper-mob' fade indicators={false} interval={2500} pauseOnHover={false}>
+      <Carousel.Item className='hero-pic1-mob'>
+          <div className='hero-align_row' >
+            <div className='hero-align_col' >
+              {/* <Carousel.Caption> */}
+              <Title className="title-car-mob">Welcome to Generations Humanitarian</Title>
+              <br></br>
+              <SubHeading className="subhead-car-mob">We build and operate orphanages and drop-in centers for street children in Latin America</SubHeading>
+              {/* </Carousel.Caption> */}
+              <br></br>
+              <Link
+              smooth to='/#about'
+              >
+              <Button>Learn More</Button>
+              </Link>
+          </div>
+        </div>
+      </Carousel.Item>
+      <Carousel.Item className='hero-pic2'>
+          <div className='hero-align_row' >
+            <div className='hero-align_col' >
+              {/* <Carousel.Caption> */}
+              <Title>Come to South America in 2019</Title>
+              <br></br>
+              <SubHeading>Contact us for a group's custom expedition</SubHeading>
+              {/* </Carousel.Caption> */}
+              <br></br>
+              <Link
+              smooth to='/#about'
+              >
+              <Button >Learn More</Button>
+              </Link>
+              
+          </div>
+        </div>
+      </Carousel.Item>
+      <Carousel.Item className='hero-pic3'>
+          <div className='hero-align_row' >
+            <div className='hero-align_col' >
+              {/* <Carousel.Caption> */}
+              <Title>Refuge Crisis</Title>
+              <br></br>
+              <SubHeading>Help Venezuela Refugee Children</SubHeading>
+              {/* </Carousel.Caption> */}
+              <br></br>
+              <Link
+              smooth to='/#about'
+              >
+              <Button>Learn More</Button>
+              </Link>
+          </div>
+        </div>
+      </Carousel.Item>
+      <Carousel.Item className='hero-pic4'>
+          <div className='hero-align_row' >
+            <div className='hero-align_col'>
+              {/* <Carousel.Caption> */}
+              <Title>Greenhouse & Hydroponics Project</Title>
+              <br></br>
+              <SubHeading>We are creating self-sustaining, scalable, water efficient, organice greenhouses 
+                that teach children a work ethic, give them price in their produce, and funds their home.</SubHeading>
+              {/* </Carousel.Caption> */}
+              <br></br>
+              <Link
+              smooth to='/#about'
+              >
+              <Button>Learn More</Button>
+              </Link>
+          </div>
+        </div>
+      </Carousel.Item>
+ 
+  </Carousel>
+  )
+}
+
+export default LandingCarousel;
